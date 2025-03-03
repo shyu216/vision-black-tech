52dbda5680a78afa11c61ca58ea948faaee0ae0c
shyu216 <sihong1@student.unimelb.edu.au>
2025-03-03 11:05:10 +1100

    Update demo.m

--- EVM_Matlab/demo.m
+++ EVM_Matlab/demo.m
@@ -14,3 +14,7 @@ amplify_spatial_lpyr_temporal_butter(inFile,resultsDir,20,80,0.5,10,30, 0);
 % Color
 amplify_spatial_Gdown_temporal_ideal(inFile,resultsDir,100,6,50/60,60/60,30, 1);
 
+inFile = fullfile(dataDir,'b5.mp4');
+fprintf('Processing %s\n', inFile);
+
+amplify_spatial_lpyr_temporal_iir(inFile, resultsDir, 10, 16, 0.5, 0.05, 0.1);
